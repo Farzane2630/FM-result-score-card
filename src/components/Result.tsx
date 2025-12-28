f3643e221fd670541630b11e8babf7960f2ffe4d
farzane <farzanekazemi8517@gmail.com>
2025-12-28 18:59:44 +0330

    feat: result component(html)

---
+++ src/components/Result.tsx
@@ -0,0 +1,23 @@
+const Result = () => {
+  return (
+    <section aria-label="result-section" id="result_container">
+      <p className="result__header">Your Result</p>
+
+      <div className="result__circle">
+        {/* calculated and replaced by state */}
+        <h1 className="result__number">76</h1>
+
+        <p className="result__total">of 100</p>
+      </div>
+
+      <div className="result__description">
+        <p className="result__description__title">Greate</p>
+        <p className="result__description__body">
+          You scored higher than 65% of the people who have taken these tests.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Result;
