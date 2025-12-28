e491c33eabb2038c7040f0ecde615bbe2a291208
farzane <farzanekazemi8517@gmail.com>
2025-12-28 18:59:05 +0330

    inittial preps and install packages

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Result from "./components/Result";
+import Summary from "./components/Summary";
+
+function App() {
+  return (
+    <>
+      <Result />
+      <Summary />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Summary.tsx
@@ -0,0 +1,7 @@
+const Summary = ()=>{
+    return (
+        <h1>This is summary component</h1>
+    )
+}
+
+export default Summary;
